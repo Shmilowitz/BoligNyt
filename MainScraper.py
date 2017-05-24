d57fd926682965861ddbc268a9f135a5efe8ef3b
Shmilowitz <sandwichmanden@hotmail.com>
2017-05-25 00:04:29 +0200

    Fixed mainscraper.py

--- MainScraper.py
+++ MainScraper.py
@@ -1,5 +1,3 @@
-# coding=utf-8
-
 import bs4
 import requests
 import warnings
@@ -7,11 +5,12 @@ import csv
 import datetime
 import sys
 import pandas as pd
-import fileinput
+from fileinput import FileInput
+from itertools import tee
 warnings.filterwarnings('ignore')
 
 def DBAscraper():
-	print("\n       ============================ \n       |        DBA Scraper       |\n       |   by David Shmilowitz    |\n       ============================ \n")
+	print("\n       ============================ \n       |        DBA Scraper       |\n       |   by David Shmilowitz    |\n       |   &    Joseph Awwal      |    \n       ============================ \n")	
 	for i in range(1,100):
 		url = "http://www.dba.dk/boliger/ejerbolig/side-{}/".format(i)
 		r = requests.get(url)
@@ -23,7 +22,7 @@ def DBAscraper():
 		for listing in listings:
 			#URL
 			link.append(listing.find('a', 'listingLink')['href'])
-			#Description
+			#Description 
 			d = listing.find('div', 'expandable-box expandable-box-collapsed')
 			#Removing the last 50 characters (Annoncen præsenteres i samarbejde med Boligsiden)
 			desc.append(d.find('a', 'listingLink').text.strip()[:-50])
@@ -37,19 +36,45 @@ def DBAscraper():
 					date[index] = ("%s/%s" % (datetime.datetime.now().day-1, datetime.datetime.now().month))
 			#Price
 			price.append(listing.find('td', {'title': 'Pris'}).text.strip())
-
+			
 		results =  list(zip(date,price,link,desc))
 
 		with open('DBA.csv', 'a', encoding='Cp1252', newline='') as f:
 			writer = csv.writer(f,delimiter=",")
 			if i == 1:
-				writer.writerow(["Date", "Price","Url","Description"])
+				writer.writerow(["Date", "Price DKK","URL","Description"])
 			for item in results:
 				writer.writerow(item)
 		print(url + " Page {} Done".format(i))
-
-
-
-
+		
+def deleteDuplicate():
+	# seen = set()
+	# dupeCount = 0
+	# for line in fileinput.FileInput('DBA.csv', inplace=1):
+		# linesplit = line.split (",")[2]
+		# if linesplit in seen:
+			# dupeCount+=1
+			# continue # skip duplicated line
+		# seen.add(linesplit)
+		# print(line, end='')
+	# print(seen)
+	# print("Removed {} Duplicates".format(dupeCount))
+	
+	seen = set()
+	dupeCount = 0
+	counter = 0
+	with FileInput('DBA.csv', inplace=1) as f:
+		f, f_orig = tee(f)
+		for row, line in zip(csv.reader(f), f_orig):
+			if row[2] in seen:
+				dupeCount+=1
+				continue
+			seen.add(row[2])
+			counter+=1
+			print(line, end='')
+		print(counter)
+		print("Removed {} Duplicates".format(dupeCount))
 
 DBAscraper()
+deleteDuplicate()
+
